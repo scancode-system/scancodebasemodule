d82dd1f3edd4a33a072fef45fb6d9a3d136a8869
Your Name <you@example.com>
2020-01-16 21:47:38 -0300

    .

---
+++ Providers/ScancodeBaseServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\ScancodeBase\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class ScancodeBaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
